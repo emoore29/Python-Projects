1ddf72c45b2b1a05a95f32ae260ae606fd0be20e
AthenasCode <emoore5203@gmail.com>
2023-11-22 13:25:51 +0800

    finished writing draw method for Hat class

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -4,13 +4,42 @@ import random
 class Hat:
       def __init__(self, **kwargs):
             self.contents = []
-            for key, value in kwargs:
-                  self.contents.append(key * value)
+            for key, value in kwargs.items():
+                  for _ in range(value):
+                        self.contents.append(key)
+                        
+      def draw(self, x):
+            # check whether x exceeds the number of balls in self.contents
+            if x > len(self.contents):
+                  return self.contents
+            else:
+                  drawn = []
+                  # draw X number of balls
+                  for _ in range(x):
+                        # generate random index to draw from based on length of self.contents
+                        random_number = random.randint(0, len(self.contents) - 1)
+                        
+                        # identify ball based on random index
+                        ball_drawn = self.contents[random_number]
+                        
+                        # remove ball from self.contents
+                        self.contents.pop(random_number)
+                        
+                        # append ball to drawn list
+                        drawn.append(ball_drawn)
+            
+                  return drawn      
                   
       def __str__(self):
             print(self.contents)
       
       
-hat = Hat(red=2, blue=4)
-print(hat)
-# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+hat = Hat(red=6, blue=4, green=2)
+
+print(hat.draw(2))
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+      
+      # draw balls from hat
+      hat.draw(num_balls_drawn)
+      
