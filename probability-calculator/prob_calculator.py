d4e7f915b88354ebb7674c33e079648ef1f28fb8
AthenasCode <emoore5203@gmail.com>
2023-11-22 12:00:13 +0800

    testing appending key/values to contents

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -4,8 +4,13 @@ import random
 class Hat:
       def __init__(self, **kwargs):
             self.contents = []
-            
+            for key, value in kwargs:
+                  self.contents.append(key * value)
+                  
+      def __str__(self):
+            print(self.contents)
       
       
-
+hat = Hat(red=2, blue=4)
+print(hat)
 # def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
